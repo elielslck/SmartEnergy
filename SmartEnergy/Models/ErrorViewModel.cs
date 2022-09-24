0d1cc01e5c560ea1d7c6070160711dfdab508f99
Eliel Oliveira <elielslck@gmail.com>
2022-09-24 12:31:31 -0300

    Delete SmartEnergy directory

--- SmartEnergy/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace SmartEnergy.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
