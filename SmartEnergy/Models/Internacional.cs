c473415ce1ec63a27103785f8006c0ff682bbd38
elielslck <elielslck@gmail.com>
2022-09-24 13:48:40 -0300

    New Commit Reload Code

---
+++ SmartEnergy/Models/Internacional.cs
@@ -0,0 +1,16 @@
+﻿namespace SmartEnergy.Models
+{
+    public class Internacional
+    {
+        public Internacional()
+        {
+            this.Argentina = 0;
+            this.Paraguai = 0;
+            this.Uruguai = 0;
+        }
+
+        public double Argentina { get; set; }
+        public double Paraguai { get; set; }
+        public double Uruguai { get; set; }
+    }
+}
